7ab2e11d3bb26bc4b28dcda184dcbe9a32ca9111
Julien QUENTIER <j.quentier@gmail.com>
2020-01-31 15:30:01 +0100

    add related products

--- wordpress/wp-content/themes/DM_Store/single-product.php
+++ wordpress/wp-content/themes/DM_Store/single-product.php
@@ -84,7 +84,7 @@ $attachment_ids =$product->get_gallery_image_ids();
       
       <div class="col-md-6 col-sm-6">
         <div class="info-right">
-          <h4>Informations Complémentaires</h4>
+          <h4>Informations Complémentaires:</h4>
           <br>
           <p>Poids: <span><?php echo $product->get_weight();?> g.</span></p>
           <p>Dimensions: <span><?php echo $product->get_dimensions();?></span></p>
@@ -97,34 +97,121 @@ $attachment_ids =$product->get_gallery_image_ids();
 </section>
 
 
+<!-- Récupération des produits liés-->
+<?php 
+        $related_products_ids = $product->get_related();
+        $product_1_ids = $related_products_ids[0];
+        $product_1 = wc_get_product($product_1_ids);
+        $product_2_ids = $related_products_ids[1];
+        $product_2 = wc_get_product($product_2_ids);
+        $product_3_ids = $related_products_ids[2];
+        $product_3 = wc_get_product($product_3_ids);
+    ?>
 
 <section id=info2>
+  <h4>Vous pourriez aimer également :</h4>
+
   <div class="container">
     <div class="row">
 
-    <div class="col-md-6 col-sm-6">
-      <div class="rewiew_form">
-        <h5>laisser un avis</h5>
+    <div class="col-md-4 col-sm-4">
+      <div class="related_products">
+        <div>
+          <a href="<?php echo get_permalink($product_1->get_id());?>">
+              <img src=<?php echo $product_1->get_image();?>
+          </a>
+          
+        </div>
+        <h6>
+          <a href=" <?php echo get_permalink($product_1->get_id());?> " class="related_product_name">
+             <?php echo $product_1->get_name();?> 
+          </a>
+        </h6>
 
-      </div>    
+        <p class="price">
+          <?php echo $product_1->get_regular_price();?> €
+        </p>
+        <p class="regular_price">
+          <?php if ( $product_1->is_on_sale() ) : ?>
+            <?php echo $product_1->get_regular_price();?> €
+          <?php endif;?>
+        </p>
+
+        <?php
+          echo '<span class="cart">';
+              $product_id = $product_1->get_id();
+              $link   = esc_url( $product_1->add_to_cart_url() );
+              $label  = apply_filters('add_to_cart_text', __('Add to cart', 'woocommerce'));
+          echo sprintf('<a href="%s" data-product_id="%s" class="btn_achat add_to_cart_button product_type_%s">Acheter</a>', $link,  $product_id->id,  $product_id->product_type, $label);
+          echo '</span>';
+      ?>
+      </div> 
+    </div>
+   
+    <div class="col-md-4 col-sm-4">
+      <div class="related_products">
+        <div>
+          <a href=""<?php echo get_permalink($product_2->get_id());?>">
+              <img src=<?php echo $product_2->get_image();?>
+          </a>
+        </div>
+        <h6>
+          <a href=" "<?php echo get_permalink($product_2->get_id());?> " class="related_product_name">
+             <?php echo $product_2->get_name();?> 
+          </a>
+        </h6>
+
+        <p class="price">
+          <?php echo $product_2->get_regular_price();?> €
+        </p>
+        <p class="regular_price">
+          <?php if ( $product_2->is_on_sale() ) : ?>
+            <?php echo $product_2->get_regular_price();?> €
+          <?php endif;?>
+        </p>
+
+        <?php
+          echo '<span class="cart">';
+              $product_id = $product_2->get_id();
+              $link   = esc_url( $product_2->add_to_cart_url() );
+              $label  = apply_filters('add_to_cart_text', __('Add to cart', 'woocommerce'));
+          echo sprintf('<a href="%s" data-product_id="%s" class="btn_achat add_to_cart_button product_type_%s">Acheter</a>', $link,  $product_id->id,  $product_id->product_type, $label);
+          echo '</span>';
+      ?>
+      </div> 
     </div>
 
-    <?php 
-        $related_products_ids = $product->get_related();
-        $product_1 = $related_products_ids[0];
-        $product_2 = $related_products_ids[1];
-        $product_3 = $related_products_ids[2];
-    ?>
+    <div class="col-md-4 col-sm-4">
+      <div class="related_products">
+        <div>
+          <a href=""<?php echo get_permalink($product_3->get_id());?>">
+              <img src=<?php echo $product_3->get_image();?>
+          </a>
+        </div>
+        <h6>
+          <a href=""<?php echo get_permalink($product_3->get_id());?>" class="related_product_name">
+             <?php echo $product_3->get_name();?> 
+          </a>
+        </h6>
 
-    <div class="col-md-6 col-sm-6">
-      <div class="rewiews">
-        <h5>Avis Clients</h5>
-        <?php echo $product_1;?> 
-        <?php echo $product_2;?> 
-        <?php echo $product_3;?> 
+        <p class="price">
+          <?php echo $product_3->get_price();?> €
+        </p>
+        <p class="regular_price">
+          <?php if ( $product_3->is_on_sale() ) : ?>
+            <?php echo $product_3->get_regular_price();?> €
+          <?php endif;?>
+        </p>
 
-        <img src=<?php echo $product_1->get_image();?>
-      </div>
+        <?php
+          echo '<span class="cart">';
+              $product_id = $product_3->get_id();
+              $link   = esc_url( $product_3->add_to_cart_url() );
+              $label  = apply_filters('add_to_cart_text', __('Add to cart', 'woocommerce'));
+          echo sprintf('<a href="%s" data-product_id="%s" class="btn_achat add_to_cart_button product_type_%s">Acheter</a>', $link,  $product_id->id,  $product_id->product_type, $label);
+          echo '</span>';
+      ?>
+      </div> 
     </div>
 
     </div>

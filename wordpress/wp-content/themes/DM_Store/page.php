d5ab7909e694a36286a64c17f45f8b412aef856a
Julien QUENTIER <j.quentier@gmail.com>
2020-02-06 20:48:47 +0100

    delete theme / début personalisation shopt-page

--- wordpress/wp-content/themes/DM_Store/page.php
+++ wordpress/wp-content/themes/DM_Store/page.php
@@ -1,4 +1,14 @@
-<?php get_header(); // appel du fichier header.php ?>
+<?php get_header(); // appel du fichier header.php 
+
+
+if ( is_product_category() ){ global $wp_query; $cat = $wp_query->get_queried_object();
+	$thumbnail_id = get_woocommerce_term_meta( $cat->term_id, 'thumbnail_id', true );
+	$image = wp_get_attachment_url( $thumbnail_id );
+	// if ( $image ) {
+	// 	echo '<img class="classtopright" src="' . $image . '" alt="" width="280"/>';
+	// }
+}
+?>
 			  <div class="container">
 				<!-- Example row of columns -->
 				<div class="row">
@@ -6,7 +16,12 @@
 				<?php if ( have_posts() ) : while ( have_posts() ) : the_post(); ?>
 				
 				<div class="col-md-12">
-				<h2><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h2>
+					<div class="title_page" style="background: url(<?php echo $image;?>) no-repeat; background-size: cover;
+			background-position: center">
+
+						<h2><a href="<?php the_permalink(); ?>"> <?php the_title(); ?></a></h2>
+						
+					</div>
 				<div class="contenu"><?php the_content(); ?></div>
 				</div>
 				<?php endwhile; else: ?>

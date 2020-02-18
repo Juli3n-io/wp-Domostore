e2807de9e5e625bdf95a506a770fc79ec1f9c046
Julien QUENTIER <j.quentier@gmail.com>
2020-02-18 15:33:04 +0100

    Création header2.0 / intégration root

--- wordpress/wp-content/themes/DM_Store/assets/js/script.js
+++ wordpress/wp-content/themes/DM_Store/assets/js/script.js
@@ -2,13 +2,26 @@ $(document).ready(function(){
   $('.menu-btn').click(function(){
     $('.navbar').slideToggle(500);
   })
-  $('.menu-btn').click(function(){
-    $('.header').toggleClass('white');
-    $('#responsive_cart').toggleClass('black');
+  // $('.menu-btn').click(function(){
+  //   $('.header').toggleClass('white');
+  //   $('#responsive_cart').toggleClass('black');
+  // })
+});
+
+$(document).ready(function(){
+  $('.search_click').click(function(){
+    $('.search_container').slideToggle(500);
   })
 });
 
+$('.menu-icon').click(function(){
+  $('.nav-icon-1').toggleClass('active')
+  $('.nav-icon-2').toggleClass('active')
+  $('.nav-icon-3').toggleClass('active')
+});
 
+const nav_icon = document.querySelector('.nav-icon');
+const nav_icon_2 = document.querySelector('.nav-icon:after')
 
 
 
